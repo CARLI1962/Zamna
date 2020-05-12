402c5dc375c0b8427f75ca79a2bda3966ead932a
segoto <js.gomez23@uniandes.edu.co>
2020-05-11 23:48:31 -0500

    autenticacion montada

--- Zamna/settings.py
+++ Zamna/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bao-uayt=(5mn%7xtws79=3kbz3oajpiw#m4@%u)1^(lf$!jec'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -101,7 +102,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+LOGIN_URL = '/login/auth0'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'https://zamna.auth0.com/v2/logout?returnTo=http%3A%2F%2Flocalhost:8000'
+SOCIAL_AUTH_TRAILING_SLASH = False
+SOCIAL_AUTH_AUTH0_DOMAIN = 'zamna.auth0.com'
+SOCIAL_AUTH_AUTH0_KEY = '0qSNaC3nOwSXBcx6Ror18VM0AfKNJQW7'
+SOCIAL_AUTH_AUTH0_SECRET = 'rfhlbY-MFAQuDWAE68sIp-zn-cJq8BhFGh7dLeAzJ293dCPAUH1rWu-6NKv6qiec'
+SOCIAL_AUTH_AUTH0_SCOPE = [ 'openid', 'profile' ]
+AUTHENTICATION_BACKENDS = { 'Zamna.auth0backend.Auth0', 'django.contrib.auth.backends.ModelBackend', }
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
